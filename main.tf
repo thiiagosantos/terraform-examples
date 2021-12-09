5914aa281296f0be69fa5525f6529eb4613b254a
Thiago Santos <thiago.lima-santos@itau-unibanco.com.br>
2021-12-08 23:40:24 -0300

    More configs

--- main.tf
+++ main.tf
@@ -1,25 +1,35 @@
 terraform {
-    required_version = "1.0.11"
-    required_providers {
-      aws = {
-          source    = "hashicorp/aws"
-          version   = "3.68.0"
-      }
+  required_version = "1.0.11"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.68.0"
     }
+  }
 }
 
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "personal-account" # qual profile de conexão será usado
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
+# define s3 bucket
 resource "aws_s3_bucket" "my-test-bucket-tls" {
-  bucket = "my-test-bucket-tls-1"
+  bucket = "my-test-bucket-tls-2"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
     ManagedBy   = "Terraform"
+    Owner       = "Thiago Santos"
   }
+}
+
+// define ec2 instance
+resource "aws_instance" "web" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  tags = var.instance_tags
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region    = "us-east-1"
+aws_profile   = "personal-account"
+instance_ami  = "ami-0a1eddae0b7f0a79f"
+instance_type = "t4g.micro"
