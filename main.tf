ef41b028f87b7166e6c8020119a46f1997830926
Thiago Santos <thiago.lima-santos@itau-unibanco.com.br>
2021-12-08 22:43:26 -0300

    First commit, containing s3 bucket and terraform lock configs

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region    = "us-east-1"
+  profile   = "personal-account" # qual profile de conexão será usado
+}
 
+resource "aws_s3_bucket" "my-test-bucket-tls" {
+  bucket = "my-test-bucket-tls-1"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
